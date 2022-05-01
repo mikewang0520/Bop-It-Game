0d7efdb77b4d92be8041a4b0fa7d87c371f33096
mikewang0520 <85894910+mikewang0520@users.noreply.github.com>
2022-05-01 17:50:11 -0500

    Add files via upload

---
+++ drawBitmaps.c
@@ -0,0 +1,150 @@
+// draw number one
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    22,
+    46,
+    numOneBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number two
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numTwoWidthPixels,
+    numTwoHeightPixels,
+    numTwoBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number three
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numThreeWidthPixels,
+    numThreeHeightPixels,
+    numThreeBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number four
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numFourWidthPixels,
+    numFourHeightPixels,
+    numFourBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number five
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numFiveWidthPixels,
+    numFiveHeightPixels,
+    numFiveBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number six
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numSixWidthPixels,
+    numSixHeightPixels,
+    numSixBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number seven
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numSevenWidthPixels,
+    numSevenHeightPixels,
+    numSevenBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number eight
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    num8WidthPixels,
+    num8HeightPixels,
+    num8Bitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw number nine
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    numNineWidthPixels,
+    numNineHeightPixels,
+    numNineBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw twist task
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    twistWidthPixels,
+    twistHeightPixels,
+    twistBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw tilt task
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    tiltWidthPixels,
+    tiltHeightPixels,
+    tiltBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+
+// draw click task
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    clickWidthPixels,
+    clickHeightPixels,
+    clickBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw flash task
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    flashWidthPixels,
+    flashHeightPixels,
+    flashBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw win condition
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    winWidthPixels,
+    winHeightPixels,
+    winBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
+
+// draw lose condition
+lcd_draw_image(
+    CENTER_ROW,
+    CENTER_COL,
+    loseWidthPixels,
+    loseHeightPixels,
+    loseBitmaps,
+    LCD_COLOR_RED,
+    LCD_COLOR_BLACK);
